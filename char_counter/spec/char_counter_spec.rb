5b09b88be0b8091a1d5fbaa7cb2a0ef255dd70c6
angeles-tellaarena <angeles.tellaarena@gmail.com>
2015-04-06 01:19:46 -0300

    Commit para test 2

--- char_counter/spec/char_counter_spec.rb
+++ char_counter/spec/char_counter_spec.rb
@@ -8,4 +8,9 @@ describe 'CharCounter' do
     dic['a'].should be 1
   end
 
+  it 'should return {b=>nil} when string is a' do
+    dic = CharCounter.count('a')
+    dic['b'].should be nil
+  end  
+  
 end
