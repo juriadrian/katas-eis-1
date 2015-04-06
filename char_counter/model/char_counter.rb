8e00c6bd70e95429137ee11f55bfd72006eb5bd4
angeles-tellaarena <angeles.tellaarena@gmail.com>
2015-04-06 01:44:30 -0300

    Commit para test 6

--- char_counter/model/char_counter.rb
+++ char_counter/model/char_counter.rb
@@ -2,14 +2,16 @@ class CharCounter
 
   def self.count(word)
     ocurrences = Hash.new
-    new_word = word.gsub(/\s+/, "").split(//)
-    new_word.each { 
-      |letter| if ocurrences.has_key?(letter)
-        ocurrences[letter] += 1 
-      else
-        ocurrences[letter] = 1
+    unless word == nil
+      new_word = word.gsub(/\s+/, "").split(//)
+      new_word.each { 
+        |letter| if ocurrences.has_key?(letter)
+          ocurrences[letter] += 1 
+        else
+          ocurrences[letter] = 1
+        end
+      }
       end
-    }
     ocurrences
   end
 
